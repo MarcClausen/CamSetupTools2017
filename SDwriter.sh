591831a3c94bfe36323f07a4bb7d6bafff4d553a
test <marcclausen@protonmail.com>
2019-08-29 13:52:33 +0200

    Update SDwriter.sh

--- SDwriter.sh
+++ SDwriter.sh
@@ -1,32 +1,47 @@
 #!/bin/sh
 clear
 sleep 2
+# Warning!
+# This script uses the DD command, which can destroy your data is used improperly
+# This includes data on internal system storage
+# I assume zero liability. You are on your own.
+
 INPUTPOINT=$1
 OUTPUTPOINT=$2
-umount $21
+#The first argument is the input file (Your Image)
+#The second argument is the device to write to (SDcard)
+
+MEGABYTES=$3
+# This is how many megabytes you wanna write 
+# Pick a number slightly higher than the amount of data to be written
+
+# EXAMPLE USAGE:
+# sh SDwriter.sh image.img /dev/mmcblk0 2500
+
+umount $2
 sync
-umount $22
+umount $2
 sync
 umount $2p1
 sync
 umount $2p2
 sync
-dd if=/dev/zero of=$OUTPUTPOINT bs=1M count=1550 status=progress conv=sync oflag=direct
-dd if=$INPUTPOINT of=$OUTPUTPOINT bs=1M count=1500 status=progress conv=sync oflag=direct
+dd if=/dev/zero of=$OUTPUTPOINT bs=1M count=$MEGABYTES status=progress conv=sync oflag=direct
+dd if=$INPUTPOINT of=$OUTPUTPOINT bs=1M count=$MEGABYTES status=progress conv=sync oflag=direct
 sync
 sleep 1
 sync
-fsck -y $21
+fsck -y $2
 sync
-fsck -y $22
+fsck -y $2
 sync
 fsck -y $2p1
 sync
 fsck -y $2p2
 sync
-fstrim -v $21
+fstrim -v $2
 sync
-fstrim -v $22
+fstrim -v $2
 sync
 fstrim -v $2p1
 sync
